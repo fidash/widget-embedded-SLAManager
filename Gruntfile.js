adb23ca7f6662bfab30ffa5b028b656ed31c0470
bgrana <bgrana@conwet.com>
2015-09-30 16:15:14 +0200

    Add compress option for releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
+        isDev: grunt.option('target') === 'release' ? '' : '-dev',
 
         copy: {
             main: {
@@ -40,8 +41,10 @@ module.exports = function (grunt) {
         compress: {
             widget: {
                 options: {
-                    mode: 'zip',
-                    archive: 'build/<%= pkg.vendor %>_<%= pkg.name %>_<%= pkg.version %>-dev.wgt'
+                  archive: 'build/<%= pkg.vendor %>_<%= pkg.name %>_<%= pkg.version %><%= isDev %>.wgt',
+                  mode: 'zip',
+                  level: 9,
+                  pretty: true
                 },
                 files: [
                     {
